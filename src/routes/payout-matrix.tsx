e76d31e2ca4ca1786bb30867c0eebc446a939e06
shilliash-hash <shilliash@gmail.com>
2026-07-25 21:27:09 +0200

    1.001 -PO REVERCIE- brakujace importy do zmienionej mechaniki
    rozpoznawania supporterow

--- src/routes/payout-matrix.tsx
+++ src/routes/payout-matrix.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery, useSuspenseQuery, queryOptions } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { getSimflyPayload, getAirportPayoutMatrix, getAllAirportPayoutMatrices } from "@/lib/simfly.functions";
 import type { AirportPayoutMatrix, PayoutMatrixCell } from "@/lib/simfly.functions";
 import { useSimflyArgs } from "@/lib/viewed-user";
