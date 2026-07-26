6e009e415be89ad666b201e390e8089b58b3824c
shilliash-hash <shilliash@gmail.com>
2026-07-26 15:57:04 +0200

    1.002 -23- Import searchAirports from simfly.functions

--- src/routes/mission.tsx
+++ src/routes/mission.tsx
@@ -1,3 +1,4 @@
+import { searchAirports } from "@/lib/simfly.functions";
 import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useServerFn } from "@tanstack/react-start";
