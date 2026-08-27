4577c09b8125073d1553c0c8dd9c1942ba6985e1
shilliash-hash <shilliash@gmail.com>
2026-08-27 08:32:06 +0200

    1.120 -2- Update tile layer URL and add attribution

--- src/components/flight-map.tsx
+++ src/components/flight-map.tsx
@@ -180,9 +180,14 @@ export function FlightMap({ hubs, flights, airplanes = [], licenses = [], liveFl
           worldCopyJump: true,
         }).setView([20, 0], 2);
         L.tileLayer(
-          "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=cb1_2a77_1_ea2b4c77037024fcc2caffa8",
-          { maxZoom: 18, subdomains: "abcd" },
-        ).addTo(map);
+  "https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png?key=cb1_2a77_1_ea2b4c77037024fcc2caffa8",
+  {
+    maxZoom: 18,
+    subdomains: "abcd",
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  }
+).addTo(map);
         mapRef.current = map;
       }
 
